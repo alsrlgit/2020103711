637c611b773d33b8d389db06446cb08115d9e642
Seung Taek <tmdxor6018@naver.com>
2021-04-13 23:59:03 +0900

    10단까지 수정

--- gugudan.cpp
+++ gugudan.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
 	//please change 9 to 10
-	for (int i = 1; i <= 9; i++) {
+	for (int i = 1; i <= 10; i++) {
 		for (int j = 1; j <= 9; j++)
 			cout << i << " * " << j << " = " << i * j << "  ";
 		cout << endl;
